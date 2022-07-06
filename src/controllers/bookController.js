59afed33580d8eaa792af5ca8477efdb49a0e51e
Sudh1 <sksinghnke1@gmail.com>
2022-07-06 16:40:29 +0530

    CreateBook

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,1 +1,68 @@
-4
+const  mongoose  = require("mongoose");
+const bookModel = require("../models/bookModel");
+const userModel = require("../models/userModel");
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    if (typeof value === "string") return true
+    
+}
+////////////////////////////////////////////////////-----CREATE BOOK-------//////////////////////////////////////////////////////////////////
+const createBook = async function (req, res) {
+    try {
+        let data = req.body;
+
+        if (!Object.keys(data).length) {
+            return res.status(400).send({ status: false, message: "You must enter data" })
+        }
+        let {title,excerpt,userId,ISBN,category,subcategory,reviews,} = data
+        if (!title) return res.status(400).send({ status: false, message: "You must enter title" })
+
+        if (!isValid(title)) {
+            return res.status(400).send({ status: false, msg: "title should not be empty" })
+        }
+         let checkTitle = await bookModel.findOne({title })
+        if (checkTitle) { return res.status(400).send({ status: false, message: "This title is already present" }) }
+
+
+        if (!excerpt) return res.status(400).send({ status: false, message: "You must enter excerpt" })
+
+        if (!isValid(excerpt)) {
+            return res.status(400).send({ status: false, msg: "excerpt should not be empty " })
+        }
+
+        if (!userId) return res.status(400).send({ status: false, message: "You must enter excerpt" })
+      
+           
+        if (mongoose.isValidObjectId(userId)) return res.status(400).send({ status: false, msg: "userId  is not valid " })
+
+        let checkUserId = await userModel.findOne({userId })
+        if (!checkUserId) { return res.status(400).send({ status: false, message: " Not valid user , please register" }) }
+
+        if (!category) return res.status(400).send({ status: false, message: "category is required" })
+
+        if (!isValid(category)) {
+            return res.status(400).send({ status: false, msg: "category should not be empty" })
+        }
+        
+        if (!subcategory) return res.status(400).send({ status: false, message: "subcategory is required" })
+
+        // if (!isValid(subcategory)) {
+        //     return res.status(400).send({ status: false, msg: "subcategory should not be empty" })
+        // }
+       
+        let created = await bookModel.create(data)
+        res.status(201).send({ status: true,message :'Success', data: created })
+
+
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, mag: err.message })
+
+    }
+}
+
+
+
+module.exports = (createBook)
---
+++ src/route/route.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("/books",bookController.createBook)
