053d2e75895e8e87785917e2be13d206b7f1ad9a
Souvik Jha <jhasouvik4@gmail.com>
2022-07-14 19:18:39 +0530

    safalta

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -65,8 +65,50 @@ const createBook = async function (req, res) {
             subcategory: subcategory,
             releasedAt: releasedAt,
         }
-        let created = await bookModel.create(reqData)
-        res.status(201).send({ status: true, message: 'Successfully Book Data is Created', data: created })
+        aws.config.update({
+            accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+            secretAccessKey: "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",
+            region: "ap-south-1"
+        })
+        
+        let uploadFile= async ( file) =>{
+           return new Promise( function(resolve, reject) {
+            // this function will upload file to aws and return the link
+            let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+        
+            var uploadParams= {
+                ACL: "public-read",
+                Bucket: "classroom-training-bucket",  //HERE
+                Key: "abc/" + file.originalname, //HERE 
+                Body: file.buffer
+            }
+        
+        
+            s3.upload( uploadParams, function (err, data ){
+                if(err) {
+                    return reject({"error": err})
+                }
+                console.log(data)
+                console.log("file uploaded succesfully")
+                return resolve(data.Location)
+            })
+
+        
+           })
+        }
+        let files= req.files
+        if(files && files.length>0){
+            //upload to s3 and get the uploaded link
+            // res.send the link back to frontend/postman
+            let uploadedFileURL= await uploadFile( files[0] )
+            reqData.bookCover= uploadedFileURL
+            let created = await bookModel.create(reqData)
+            res.status(201).send({ status: true, message: 'Successfully Book Data is Created', data: created })
+        }
+        else{
+            res.status(400).send({ msg: "No file found" })
+        }
+
     }
     catch (err) {
         console.log(err)
